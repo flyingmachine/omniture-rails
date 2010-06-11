d3c3f57293acded5f6c53dc1a9f6caf6a95c531a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2010-06-11 11:59:55 -0400

    slight refactoring

--- lib/omniture-rails.rb
+++ lib/omniture-rails.rb
@@ -12,20 +12,23 @@ module OmnitureRails
     end
     
     def values_for(input, selector_config, priority_values, mapper_context)
-      case selector_config
-      when String
-        possible_file_path = File.join(OmnitureRails.config.sc_directory, selector_config)
-        selector_config = File.read(possible_file_path) if File.exists?(possible_file_path)
-        tree = OmnitureRails::Parser.new(selector_config).to_tree
-      when OmnitureRails::Node
-        tree = selector_config
-      end
-      
+      tree = tree_for_selector_config(selector_config)      
       values = OmnitureRails::Applier.new(input, tree, priority_values, mapper_context).result
       OmnitureRails.config.prop_map.each do |business_name, prop_name|
         values[prop_name] = values.delete(business_name)
       end
       values
+    end
+    
+    def tree_for_selector_config(selector_config)
+      case selector_config
+      when String
+        possible_file_path = File.join(OmnitureRails.config.sc_directory, selector_config)
+        selector_config = File.read(possible_file_path) if File.exists?(possible_file_path)
+        OmnitureRails::Parser.new(selector_config).to_tree
+      when OmnitureRails::Node
+        selector_config
+      end
     end
   end
 end
