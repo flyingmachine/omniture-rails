49b6698cf903b6f8a2d2ec68a1704610b32bce49
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2010-06-11 11:50:12 -0400

    Allow 'integration' method to use existing trees

--- lib/omniture-rails.rb
+++ lib/omniture-rails.rb
@@ -12,9 +12,15 @@ module OmnitureRails
     end
     
     def values_for(input, selector_config, priority_values, mapper_context)
-      possible_file_path = File.join(OmnitureRails.config.sc_directory, selector_config)
-      selector_config = File.read(possible_file_path) if File.exists?(possible_file_path)
-      tree = OmnitureRails::Parser.new(selector_config).to_tree
+      case selector_config
+      when String
+        possible_file_path = File.join(OmnitureRails.config.sc_directory, selector_config)
+        selector_config = File.read(possible_file_path) if File.exists?(possible_file_path)
+        tree = OmnitureRails::Parser.new(selector_config).to_tree
+      when OmnitureRails::Node
+        tree = selector_config
+      end
+      
       values = OmnitureRails::Applier.new(input, tree, priority_values, mapper_context).result
       OmnitureRails.config.prop_map.each do |business_name, prop_name|
         values[prop_name] = values.delete(business_name)
